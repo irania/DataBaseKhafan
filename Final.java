dc2bc5f0dfdb87f2cb4346a22fc9d8bec3b5d02b
Atefeh Irani <atefeh.ir72@gmail.com>
2013-03-04 13:37:16 -0800

    main class

---
+++ Final.java
@@ -0,0 +1,154 @@
+import java.io.File;
+import java.util.ArrayList;
+
+
+
+public class Final {
+
+	public static void main(String[] args) {
+		RunDatabase a = new RunDatabase();
+		//a.insert("deletedCompanionsOfJehu.txt");
+	
+		
+		a.insert("hello");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+//		
+		a.insert("salam");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("atieh");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("atefeh");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("maryam");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+//
+		a.insert("abbas");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("asal");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("ali");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("bad");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+//////
+//		
+		a.insert("bini");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("zamani");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("ahhhh");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("chonke");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("shir");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+//////
+//		
+		a.insert("cher");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+//
+		a.insert("you");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("xray");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("ahmagh");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("akbar");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("roxana");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+//////		nodes.clear();
+		a.insert("yavar");
+//////		nodes.add(a.getRoot());
+//////		a.print(nodes);
+////		nodes.clear();
+		a.insert("zood");
+////		nodes.add(a.getRoot());
+////		a.print(nodes);
+////		nodes.clear();
+		a.insert("riazi");
+////		nodes.add(a.getRoot());
+////		a.print(nodes);
+////		nodes.clear();
+		a.insert("rooiesh");
+////		nodes.add(a.getRoot());
+////		a.print(nodes);
+////		nodes.clear();
+//	
+		a.printBPlusTree();
+//		a.find("rooiesh");
+//		a.find("rooiesh");
+//		a.find("rooiesh");
+//		a.find("sssss");
+		a.find("riazi");
+//		a.find("zood");
+//		a.find("ali");
+//		a.printSplayTree();
+		
+////
+////		a.delete("rooiesh");//age kalame nabood!
+		//a.delete("riazi");//age kalame nabood!
+////		a.delete("zood");//age kalame na
+////		a.delete("yavar");//age kalame na
+////		a.delete("roxana");
+////		a.delete("akbar");
+////		a.delete("ahmagh");
+////		a.delete("xray");
+////		a.delete("you");
+////		a.delete("cher");
+////		a.delete("shir");
+////		a.delete("ahhhhh");
+////		a.delete("zamani");
+////		a.delete("bini");
+////		a.delete("bad");
+////		a.delete("ali");
+////		a.delete("abbas");
+////		a.delete("maryam");
+////		a.delete("atefeh");
+//		a.delete("atieh");
+//		a.delete("rooiesh");//
+//		a.delete("roxana");
+////		a.delete("hello");
+//		
+}
+
+}
